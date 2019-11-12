c05a1563e0526c1225ad6a9588bc883d1bb6c1dd
Dev Baweja <18ucs081@lnmiit.ac.in>
2019-11-12 20:43:32 +0530

    StateTable using original array

---
+++ src/StateTable.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class StateTable {
+
+	public static void main(String[] args) {
+	System.out.println();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","");
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("create database if not exists state");
+			stmt.execute("use state");
+			// Tables exists from cities.sql
+			ResultSet rscount = stmt.executeQuery("select count(distinct city_state) as count from cities");
+			rscount.next();
+			String original[] = new String[rscount.getInt("count")];
+			
+			ResultSet rs = stmt.executeQuery("select distinct city_state from cities");
+			int i=0;
+			while(rs.next())
+			{
+				original[i] = rs.getString("city_state");
+				i++;
+			}
+			
+			stmt.execute("use states");
+			for (int j = 0; j < original.length; j++) 
+			{
+				PreparedStatement pre = con.prepareStatement("insert into _statestb values(?)");
+				pre.setString(1, original[j]);
+				System.out.println(original[j]);
+				pre.executeUpdate();
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
